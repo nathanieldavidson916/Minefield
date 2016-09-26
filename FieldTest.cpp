1d3cfc8231141c16e61f7e5f53f1ad0f914ff5b6
Nathan Davidson <ndavidson4@mail.csuchico.edu>
2016-09-26 11:57:39 -0700

    found bug in isSafe function. See documentation about the bug.

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -22,3 +22,28 @@ TEST(FieldTest, placeMineInBounds)
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
 }
+
+TEST(FieldTest, isAreaSafeShouldBeFalse){
+  Field mineField;
+  
+  mineField.placeMine(4, 5);
+  ASSERT_FALSE(mineField.isSafe(4, 5));
+}
+
+TEST(FieldTest, isAreaSafeShouldBeTrue){
+  Field mineField;
+  
+  mineField.placeMine(5, 5);
+  ASSERT_TRUE(mineField.isSafe(2, 2));
+}
+
+TEST(FieldTest, isAreaSafeOutOfBoundsWithMinePlaced){
+  Field mineField;
+  mineField.placeMine(10, 10);
+  ASSERT_TRUE(mineField.isSafe(20, 20));
+}
+TEST(FieldTest, isAreaSafeOutOfBoundsWithOutMinePlaced){
+  Field mineField;
+
+  ASSERT_TRUE(mineField.isSafe(3, 2));
+}
--- buglog.md
+++ buglog.md
@@ -1,1 +1,5 @@
 # BUG LOG
+
+- Receive a segmentation fault whenever I try to check if the area 'isSafe'
+  without placing a mine first. It also happens when the col, row section is
+  out of bounds even when an exception should be thrown.
